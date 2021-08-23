0643e3ff37e7297f0291d049d4fc2e61edd4b3ec
HaleyJ <563565798@qq.com>
2021-08-23 09:57:02 +0800

    master 2 commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("000");
         System.out.println("111");
+        System.out.println("master");
     }
 }
